16925494e3960f4609195dbe189dae0507880bc9
jordanburke <jordan.burke@gmail.com>
2014-08-10 22:46:49 -0400

    Changed to use multiline strings.

--- src/main/scala/sapienapps/play/mail/AppMailService.scala
+++ src/main/scala/sapienapps/play/mail/AppMailService.scala
@@ -3,11 +3,21 @@ package sapienapps.play.mail
 
 case class AppMailService(appReply: String) extends MailService(appReply) {
 
-  val resetText = s"We received a password reset request from this email address. If you did not reset your password, contact us at ${appReply}"
-    ".\n\n\nClick below to reset your password.\n\n\n"
+  val resetText =
+    s"""We received a password reset request from this email address. If you did not reset your password, contact us at ${appReply}
+       |
+       |Click below to reset your password:
+       |
+       |
+     """.stripMargin
 
-  val resetError = "We received a password reset request from this email address but you are not in our database. Please contact us for any further inquiries\n\n\n\n" +
-    appReply
+  val resetError =
+    s"""We received a password reset request from this email address but you are not in our database. Please contact us for any further inquiries
+       |
+       |${appReply}
+       |
+     """.stripMargin
+
 
   def newUser(to: String, subject: String, content: Any) {
     mail(to, appReply, subject, content)
@@ -20,5 +30,4 @@ case class AppMailService(appReply: String) extends MailService(appReply) {
   def resetError(to: String, subject: String = "Password Reset", content: Any = resetError) {
     mail(to, appReply, subject, content)
   }
-
 }
